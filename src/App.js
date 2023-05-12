ff3383a8a70a5276eef7f1a5c4b1affb895b6aaf
raghuram <raghu.ram.y.16@gmail.com>
2023-05-12 16:01:00 +0530

    added files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>list of prabash movies</h1>
+      <h2>project k</h2>
+      <h2>salar</h2>
+      <h2>adhipurush</h2>
+    </div>
+  );
+}
+
+export default App;
